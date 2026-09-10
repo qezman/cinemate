1927d33a9dee89b3b9c81d23351cccb670daf7cf
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-09-10 11:13:48 +0100

    feat: add MovieCard and MovieGrid components

---
+++ components/movies/MovieGrid.tsx
@@ -0,0 +1,16 @@
+import { MovieCard } from "./MovieCard";
+import type { Movie } from "@/types/movie";
+
+export function MovieGrid({ movies }: { movies: Movie[] }) {
+  if (movies.length === 0) {
+    return <p className="text-text-soft">No films to show yet.</p>;
+  }
+
+  return (
+    <div className="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-4">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
+}
